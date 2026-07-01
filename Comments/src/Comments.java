ecbf228fc6dd5403ce8feed77a5b469e85dcf9a5
rohitgitprofile <rohitmailaddress@gmail.com>
2026-07-01 23:47:23 +0530

    Add files via upload

---
+++ Comments/src/Comments.java
@@ -0,0 +1,6 @@
+public class Comments{
+    public static void main(String[] args){
+        // this is a comment
+        System.out.println("Comments");
+    }
+}
---
+++ Vairiable/src/Variable.java
@@ -0,0 +1,6 @@
+public class Variable{
+    public static void main(String[] args){
+        int money = 200;
+        System.out.println(money);
+    }
+}
