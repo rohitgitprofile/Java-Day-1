326b4483141647373a8d9487184c7ca7e3ca1653
rohitgitprofile <rohitmailaddress@gmail.com>
2026-08-11 18:25:53 +0530

    Add files via upload

---
+++ MultipleLineComments/src/MultipleLineComments.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MultipleLineComments{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        /*Multiple Line
+        comments
+         */
+        System.out.println("Multiple Line Comments");
+    }
+}
